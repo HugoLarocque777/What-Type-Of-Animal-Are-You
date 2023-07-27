81503c0d362f5ecd5d87650e0abd0d7e2a29b3b8
Hugo <140665128+HugoLarocque777@users.noreply.github.com>
2023-07-27 12:33:02 -0600

    Add files via upload

---
+++ animalprogram.py
@@ -0,0 +1,156 @@
+print ("What is your name?")
+name = input()
+print ("Hello, " + name)
+print ("Just answer these next questions to find out what kind of animal you are.")
+print ("Type your answer in lower case letters")
+print ("What would you do if you where attacked by a bull?")
+print ("A = Run")
+print ("B = Try to tame the bull")
+print ("C =  Hide")
+print ("D = Attack the bull")
+print ("Type in the letter.")     
+answer1 = input()
+print ("What would you do if you where about to be run over by a car?")
+print ("A = Run")
+print ("B = Roll under the car")
+print ("C = Scream as loud as you can")
+print ("D = Jump over the car")
+print ("Type in the letter.")     
+answer2 = input()
+print ("What would you do if you stuck in a quicksand pit?")
+print ("A = squirm")
+print ("B = keep calm and hang on")
+print ("C = scream and wave")
+print ("D = accept your death and write a will")
+answer3 = input()
+if answer1=='a' and answer2=='a' and answer3=='a':
+        print ("You are a cowardly cat")
+if answer1=='a' and answer2=='a' and answer3=='b':
+        print ("You are a smart pig")
+if answer1=='a' and answer2=='a' and answer3=='c':
+        print ("You are a panicky fish")
+if answer1=='a' and answer2=='a' and answer3=='d':
+        print ("You are a deer that gives up all hope")
+if answer1=='a' and answer2=='b' and answer3=='a':
+        print ("you are a not very smart fish")
+if answer1=='a' and answer2=='b' and answer3=='b':
+        print ("You are a smart and calm dolophin")
+if answer1=='a' and answer2=='b' and answer3=='c':
+        print ("You are a crazy horse")
+if answer1=='a' and answer2=='b' and answer3=='d':
+        print ("You are a leapard")
+if answer1=='a' and answer2=='c' and answer3=='a':
+        print ("You are a mouse")
+if answer1=='a' and answer2=='c' and answer3=='b':
+        print ("You are a human")
+if answer1=='a' and answer2=='c' and answer3=='c':
+        print ("You are a dog ")
+if answer1=='a' and answer2=='c' and answer3=='d':
+        print ("You are a star treck space elf")
+if answer1=='a' and answer2=='d' and answer3=='a':
+        print ("You are a dwarf from LOTR")
+if answer1=='a' and answer2=='d' and answer3=='b':
+        print ("You are a tree ent from LOTR")
+if answer1=='a' and answer2=='d' and answer3=='c':
+        print ("You are a puny ant")
+if answer1=='a' and answer2=='d' and answer3=='d':
+        print ("You are a tiger")
+if answer1=='b' and answer2=='a' and answer3=='a':
+        print ("You are a fat worm")
+if answer1=='b' and answer2=='a' and answer3=='b':
+        print ("You are a caterpiller ")
+if answer1=='b' and answer2=='a' and answer3=='c':
+        print ("You are a spider")
+if answer1=='b' and answer2=='a' and answer3=='d':
+        print ("You are a slow thinking dog")
+if answer1=='b' and answer2=='b' and answer3=='a':
+        print ("You are an eagle")
+if answer1=='b' and answer2=='b' and answer3=='b':
+        print ("You are a parrot")
+if answer1=='b' and answer2=='b' and answer3=='c':
+        print ("You are a worthog")
+if answer1=='b' and answer2=='b' and answer3=='d':
+        print ("You are a llama")
+if answer1=='b' and answer2=='c' and answer3=='a':
+        print ("You are a Zebra")
+if answer1=='b' and answer2=='c' and answer3=='b':
+        print ("You are a crocodile")
+if answer1=='b' and answer2=='c' and answer3=='c':
+        print ("You are a slimy, sneaking, snake.")
+if answer1=='b' and answer2=='c' and answer3=='d':
+        print ("You are an earth worm")
+if answer1=='b' and answer2=='d' and answer3=='a':
+        print ("You are a koala")
+if answer1=='b' and answer2=='d' and answer3=='b':
+        print ("You are a kangaroo")
+if answer1=='b' and answer2=='d' and answer3=='c':
+        print ("You are a Tasmanian Devel")
+if answer1=='b' and answer2=='d' and answer3=='d':
+        print ("You are a stuborn donkey")
+if answer1=='c' and answer2=='a' and answer3=='a':
+        print ("You are a butterfly")
+if answer1=='c' and answer2=='a' and answer3=='b':
+        print ("You are a monkey")
+if answer1=='c' and answer2=='a' and answer3=='c':
+        print ("You are a orangatang")
+if answer1=='c' and answer2=='a' and answer3=='d':
+        print ("You are a daddy-long-leg spider")
+if answer1=='c' and answer2=='b' and answer3=='a':
+        print ("You are a swamp monster")
+if answer1=='c' and answer2=='b' and answer3=='b':
+        print ("You are an alien from mars")
+if answer1=='c' and answer2=='b' and answer3=='c':
+        print ("You are a rainbow trout")
+if answer1=='c' and answer2=='b' and answer3=='d':
+        print ("You are a polar bear")
+if answer1=='c' and answer2=='c' and answer3=='a':
+        print ("You are a grizzly bear")
+if answer1=='c' and answer2=='c' and answer3=='b':
+        print ("You are a huge blue whale")
+if answer1=='c' and answer2=='c' and answer3=='c':
+        print ("You are a great white shark")
+if answer1=='c' and answer2=='c' and answer3=='d':
+        print ("You are a leapard")
+if answer1=='c' and answer2=='d' and answer3=='a':
+        print ("You are a Wolf")
+if answer1=='c' and answer2=='d' and answer3=='b':
+        print ("You are a Coyote")
+if answer1=='c' and answer2=='d' and answer3=='c':
+        print ("You are a bullet shrimp")
+if answer1=='c' and answer2=='d' and answer3=='d':
+        print ("You are a gorila")
+if answer1=='d' and answer2=='a' and answer3=='a':
+        print ("You are a Crab")
+if answer1=='d' and answer2=='a' and answer3=='b':
+        print ("You are a Catfish")
+if answer1=='d' and answer2=='a' and answer3=='c':
+        print ("You are a lynx")
+if answer1=='d' and answer2=='a' and answer3=='d':
+        print ("You are a hedgehog")
+if answer1=='d' and answer2=='b' and answer3=='a':
+        print ("You are a porky pine.")
+if answer1=='d' and answer2=='b' and answer3=='b':
+        print ("You are an ant eater")
+if answer1=='d' and answer2=='b' and answer3=='c':
+        print ("You are a little insignificant ant")
+if answer1=='d' and answer2=='b' and answer3=='d':
+        print ("You are an ameboa")
+if answer1=='d' and answer2=='c' and answer3=='a':
+        print ("You are a bottle nosed doliphin")
+if answer1=='d' and answer2=='c' and answer3=='b':
+        print ("You are a bull with pointy horns")
+if answer1=='d' and answer2=='c' and answer3=='c':
+        print ("You are a milk cow")
+if answer1=='d' and answer2=='c' and answer3=='d':
+        print ("You are a dingo")
+if answer1=='d' and answer2=='d' and answer3=='a':
+        print ("You are a rattle snake")
+if answer1=='d' and answer2=='d' and answer3=='b':
+        print ("You are a great big toad")
+if answer1=='d' and answer2=='d' and answer3=='c':
+        print ("You are a giraffe")
+if answer1=='d' and answer2=='d' and answer3=='d':
+        print ("You are a puffer fish")
+
+        
+
